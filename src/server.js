f2e7441e7164b37c8dd6ab7513a6f43749aa3d8a
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-10-12 22:19:04 -0300

    escondi as vars de ambiente

--- src/server.js
+++ src/server.js
@@ -30,11 +30,11 @@ async function connect(){
     if(global.connection) return global.connection.connect()
     else {
         const pool = new Pool({
-            host: 'silly.db.elephantsql.com',
-            user: 'fdpgvqke',
-            database: 'fdpgvqke',
-            password: 'jEzhdo-wl5KoCAr5wGJVoy3CxLhqkkJT',
-            port: 5432
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            database: process.env.DATABASE_DB,
+            password: process.env.DATABASE_PASSWORD,
+            port: process.env.DATABASE_PORT
         });
         global.connection = pool
         return pool.connect()
